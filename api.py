39e614154f6225bf6bd9f2e34385a6f025c1db3b
firecracker <superzap1337@gmail.com>
2022-10-12 05:09:33 +0300

    Streaming response and background tasks

--- api.py
+++ api.py
@@ -1,27 +1,37 @@
 import shutil
+from uuid import uuid4
+from starlette.templating import Jinja2Templates
+from starlette.responses import StreamingResponse
 from typing import List
-from fastapi import UploadFile, File, APIRouter, Form
+from fastapi import UploadFile, File, APIRouter, Form, HTTPException, BackgroundTasks
 from schemas import UploadVideo, Message
 from models import Video, User
+from services import save_video
 
 
 video_router = APIRouter()
 
+templates = Jinja2Templates(directory="templates")
+
 
 @video_router.post('/')
 async def create_video(
-        title: str = Form(...), description: str = Form(...), file: UploadFile = File(...)
+        back_tasks: BackgroundTasks,
+        title: str = Form(...),
+        description: str = Form(...),
+        file: UploadFile = File(...),
 ):
     # Укакзываем валидатор
-    info = UploadVideo(title=title, description=description)
-    with open(f'{file.filename}', 'wb') as buffer:
-        shutil.copyfileobj(file.file, buffer)
+
     user = await User.objects.first()
-    return await Video.objects.create(file=file.filename, user=user, **info.dict())
+    return await save_video(user, file, title, description, back_tasks)
 
 
 @video_router.get("/video/{video_pk}", response_model=Video, responses={404: {"model": Message}})
-async def get_video(video_pk):
-    return await Video.objects.select_related('user').get(pk=video_pk)
+async def get_video(video_pk: int):
+    file = await Video.objects.select_related('user').get(pk=video_pk)
+    file_like = open(file.dict().get('file'), mode='rb')
+    # Показываем ролик
+    return StreamingResponse(file_like, media_type="video/mp4")
 
 
